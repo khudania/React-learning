10907caa65187ee794a15f897dc5331f9ba98097
Himanshu Khudania <hkhudania@gmail.com>
2020-02-20 22:58:27 +0530

    Added the basic Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi How are you?</h1>
+        <Person />
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'I am React!!!'));
