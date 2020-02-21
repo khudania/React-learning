89c93598374039e9a653cd885a93cc6dc3cfa6c1
Himanshu Khudania <hkhudania@gmail.com>
2020-02-21 12:44:51 +0530

    Learned about Props and states and How to update props from state onclick

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Manya', age: 38 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log("Was Clicked");
+    //Don't do this:  this.state.persons[0].name = 'Hello'
+    this.setState({
+      persons: [
+        { name: 'Ajay', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Manya', age: 27 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi How are you?</h1>
-        <button>Switch Name</button>
-        <Person name="Max" age="28" />
-        <Person name="Manu" age="38">My Hobby is Racing</Person>
-        <Person name="Manya" age="25" />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobby is Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'I am React!!!'));
