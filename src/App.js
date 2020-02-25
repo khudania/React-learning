2510e6c90b44f8e95959cda618629ac2b9329b83
Himanshu Khudania <hkhudania@gmail.com>
2020-02-25 21:33:53 +0530

    Parsing method reference between components - 1

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ class App extends Component {
     otherState: 'some other value'
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
     this.setState({
       persons: [
-        { name: 'Maximilian', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 }
       ]
@@ -30,7 +30,8 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={this.switchNameHandler.bind(this, 'Himanshu')}>Switch Name</button>
+        {/* <button onClick={() => this.switchNameHandler('Himanshu123)}>Switch Name</button> */}
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -38,7 +39,7 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler}
+          click={this.switchNameHandler.bind(this, 'Himanshu0705')}
         >
           My Hobbies: Racing
         </Person>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,6 +5,7 @@ const person = (props) => {
         <div>
             <p onClick={props.click}>I am a {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
+
         </div>
     )
 };
