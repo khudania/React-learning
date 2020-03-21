50692ee18cc31b7745d4c028460a94dc170924bf
Himanshu Khudania <hkhudania@gmail.com>
2020-03-21 12:02:56 +0530

    Learned Outputting Lists

--- src/App.js
+++ src/App.js
@@ -55,7 +55,10 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
+          {this.state.persons.map(person => {
+            return <Person name={person.name} age={person.age} />
+          })}
+          {/* <Person
             name={this.state.persons[0].name}
             age={this.state.persons[0].age}
           />
@@ -70,7 +73,7 @@ class App extends Component {
           <Person
             name={this.state.persons[2].name}
             age={this.state.persons[2].age}
-          />
+          /> */}
 
         </div>
       );
