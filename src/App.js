d92306ea0b26de5678efad659229160c908e665b
Himanshu Khudania <hkhudania@gmail.com>
2020-03-21 20:45:32 +0530

    using dynamic classes styl;e changing

--- src/App.js
+++ src/App.js
@@ -111,10 +111,18 @@ class App extends Component {
       style.backgroundColor = 'red'
     }
 
+    let classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold')
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
