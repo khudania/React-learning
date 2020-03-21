8d3e2186b35c506a2540d92a6bd8ce9bdfd6c46f
Himanshu Khudania <hkhudania@gmail.com>
2020-03-21 19:21:30 +0530

    Flexible Lists

--- src/App.js
+++ src/App.js
@@ -25,16 +25,34 @@ class App extends Component {
   //   });
   // };
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Himanshu', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ],
-      otherState: 'some other value',
-      showPersons: false
+  nameChangedHandler = (event, id) => {
+
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // const person = Object.assign({}, this.state.persons[personIndex])
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons]
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons })
+
+    // this.setState({
+    //   persons: [
+    //     { name: 'Himanshu', age: 28 },
+    //     { name: event.target.value, age: 29 },
+    //     { name: 'Stephanie', age: 27 }
+    //   ],
+    //   otherState: 'some other value',
+    //   showPersons: false
+    // });
   }
 
   deletePersonHandler = (personIndex) => {
@@ -67,7 +85,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key={person.id} />
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
           {/* <Person
             name={this.state.persons[0].name}
