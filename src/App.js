d056604d55840ab028e6ef211cf20f31053a7030
Himanshu Khudania <hkhudania@gmail.com>
2020-03-21 23:47:02 +0530

    Worked on CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 // import Radium, { StyleRoot } from 'radium';
 // import styled from 'styled-components';
 import Person from './Person/Person';
@@ -83,19 +83,22 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      ':hover': {
-        backgroundColor: 'Lightgreen',
-        color: 'black'
-      }
-    };
+    // const style = {
+    //   backgroundColor: 'green',
+    //   color: 'white',
+    //   font: 'inherit',
+    //   border: '1px solid blue',
+    //   padding: '8px',
+    //   ':hover': {
+    //     backgroundColor: 'Lightgreen',
+    //     color: 'black'
+    //   }
+    // };
 
     let persons = null;
+
+    let btnClasses = ''
+
 
     if (this.state.showPersons) {
       persons = (
@@ -132,22 +135,25 @@ class App extends Component {
       //   backgroundColor: 'salmon',
       //   color: 'black'
       // };
+
+      btnClasses = classes.Red
+
     }
 
-    let classes = [];
+    let assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold')
+      assignedClasses.push(classes.bold)
     }
 
     return (
       // <StyleRoot>
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button className="button" alt={this.state.showPersons}
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
+        <button className={btnClasses} alt={this.state.showPersons}
           // style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,20 +1,20 @@
 import React from 'react';
-// import './Person.css';
+import classes from './Person.css';
 // import Radium from 'radium';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-const StyledDiv = styled.div`
-            width:60%;
-            margin:16px auto;
-            border: 1px solid #eee;
-            box-shadow: 0 2px 2px #eee;
-            padding: 16px;
-            text-align: center;
+// const StyledDiv = styled.div`
+//             width:60%;
+//             margin:16px auto;
+//             border: 1px solid #eee;
+//             box-shadow: 0 2px 2px #eee;
+//             padding: 16px;
+//             text-align: center;
 
-            '@media (min-width:500px)': {
-                width: '450px',
-            }
-        `
+//             '@media (min-width:500px)': {
+//                 width: '450px',
+//             }
+//         `
 
 const person = (props) => {
 
@@ -26,11 +26,11 @@ const person = (props) => {
 
     return (
         // <div className="Person" style={style}>
-        <StyledDiv>
+        <div className={classes.Person}>
             < p onClick={props.click} > I am a {props.name} and I am {props.age} years old</p >
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-        </StyledDiv>
+        </div>
     )
 };
 
