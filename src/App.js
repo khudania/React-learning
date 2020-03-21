cd7f131dd03349ac124f491a5ed39014930372b0
Himanshu Khudania <hkhudania@gmail.com>
2020-03-21 20:35:04 +0530

    dynamic styling

--- src/App.js
+++ src/App.js
@@ -69,7 +69,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px'
@@ -107,6 +108,7 @@ class App extends Component {
 
         </div>
       );
+      style.backgroundColor = 'red'
     }
 
     return (
